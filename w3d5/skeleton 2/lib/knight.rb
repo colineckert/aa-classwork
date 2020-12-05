c1fab2a008575ef847e8cb674f11a4c4501cb735
colineckert <colineckert10@gmail.com>
2020-12-04 18:55:05 -0800

    Updated project folder to latest commit

--- w3d5/skeleton 2/lib/knight.rb
+++ w3d5/skeleton 2/lib/knight.rb
@@ -1,34 +1,44 @@
+require_relative "00_tree_node"
+require "byebug"
 
-class KnightPathFinder
     #self.root_node(pos)  @root_node = PolyTreeNode.new(pos)
     #build_move_tree used to traverse the tree 
     #build_move_tree only used when #find_path is called
     #::valid_moves(pos) => returns an array, needs to check with @considered_positions
 
+class KnightPathFinder
+
     self.valid_moves(pos)
+        debugger
         x = pos[0]
         y = pos[1]
-        
-        possible_moves = [ [2, 1], [1, 2], [-2, 1], [-2, -1], [-1, 2], [2, -1], [1, -2], [-1, -2] ]
+
+        possible_moves = [[2, 1], [1, 2], [-2, 1], [-2, -1], [-1, 2], [2, -1], [1, -2], [-1, -2]]
 
         possible_moves.map! do |pair|
             pair[0] += x
             pair[1] += y
             pair
-        end
+        end 
 
         possible_moves.select! do |pair|
-            pair[0] >= 0 && pair[0] < 8 && pair[1] >= 0 && pair[1] < 8
+            (pair[0] >= 0 && pair[0] < 8) && (pair[1] >= 0 && pair[1] < 8)
         end
-                    
     end
 
     def initialize(pos)
+        debugger
         @root_node = PolyTreeNode.new(pos)
         @considered_positions = [@root_node]
+
+    end
+
+    def new_moves_position(pos)
+        if !@considered_positions.include?(pos)
     end
 
 end
 
 test = KnightPathFinder.new([0, 0])
+
 p test
