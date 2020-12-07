4768470ca47876f5b76fc963c0e015058510e074
colineckert <colineckert10@gmail.com>
2020-12-07 15:02:32 -0800

    Work on #children

--- w4d1/Tic-Tac-Toe_AI/lib/tic_tac_toe_node.rb
+++ w4d1/Tic-Tac-Toe_AI/lib/tic_tac_toe_node.rb
@@ -1,8 +1,9 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :board, :next_mover_mark
-  attr_accessor :prev_move_pos
+  attr_reader :board
+  attr_accessor :prev_move_pos, :next_mover_mark
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @next_mover_mark = next_mover_mark
     @prev_move_pos = prev_move_pos
@@ -22,7 +23,9 @@ class TicTacToeNode
       row.each.with_index do |col, col_idx|
         pos = [row_idx, col_idx]
         if @board.empty?(pos)
-          @board[pos] = TicTacToeNode.new(Board.new, next_mover_mark)
+          board_state = @board.dup
+          board_state[pos] = @next_mover_mark
+          TicTacToeNode.new(board_state, @prev_move_pos)
         end
       end
     end
