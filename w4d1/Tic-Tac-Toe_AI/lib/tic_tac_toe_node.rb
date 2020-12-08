d0356ad2c5be58a004117bb815005f2698d4a360
colineckert <colineckert10@gmail.com>
2020-12-07 16:38:13 -0800

    Finish #children

--- w4d1/Tic-Tac-Toe_AI/lib/tic_tac_toe_node.rb
+++ w4d1/Tic-Tac-Toe_AI/lib/tic_tac_toe_node.rb
@@ -1,3 +1,4 @@
+require "byebug"
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
@@ -19,28 +20,23 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    @next_mover_mark = (@next_mover_mark == :x ? :o : :x)
+    
     arr= []
     @board.rows.each.with_index do |row, row_idx|
       row.each.with_index do |col, col_idx|
         pos = [row_idx, col_idx]
         if @board.empty?(pos)
+
           board_state = @board.dup
-          if @prev_move_pos.nil?
-            board_state[pos] = @next_mover_mark
-          else
-            @next_mover_mark = (@next_mover_mark == :x ? :o : :x)  
-            board_state[pos] = @next_mover_mark     
-          end
-
+          board_state[pos] = @next_mover_mark
           @prev_move_pos = pos
-          # 
-          # @next_mover_mark = @board[@prev_move_pos]
-
-
+          
           arr << TicTacToeNode.new(board_state, @next_mover_mark, @prev_move_pos)
         end
       end
     end
+      
     arr
   end
 end
