d00641868440d59f5c931ea4807141b0fb6f1508
chris <chrispikacho@gmail.com>
2020-12-08 17:00:14 -0800

    Added multiple methods to Piece class

--- w4d2/chess/piece.rb
+++ w4d2/chess/piece.rb
@@ -8,6 +8,33 @@ class Piece
         @board = board
         @pos = pos
     end
+
+    def to_s
+        self.color.to_s
+    end
+
+    def empty?
+        return true if pos == nil
+        false
+    end
+
+    def valid_moves
+        positions = []
+        grid = @board.grid
+        grid.each do |row|
+            row.each do |col|
+                piece = grid[row][col]
+                if piece.color != self.color || piece.is_a?(NullPiece) 
+                    positions << [row, col]
+                end
+            end
+        end
+        positions
+    end
+
+    def pos=(val)
+        @pos = val
+    end
 end
 
 class NullPiece < Piece
@@ -17,3 +44,4 @@ class NullPiece < Piece
         super('', nil, nil)
     end
 end
+
