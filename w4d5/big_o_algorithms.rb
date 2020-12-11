343d6adc839baa74681397a3853e165e86605aed
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-11 11:59:30 -0800

    finished big-o-algos.rb

--- w4d5/big_o_algorithms.rb
+++ w4d5/big_o_algorithms.rb
@@ -30,5 +30,64 @@ def largest_contiguous_subsum1(list)
     (1...sub_sums.length).inject(sub_sums[0].sum) { |acc, el| sub_sums[el].sum > acc ? sub_sums[el].sum : acc }
 end
 
+def largest_contiguous_subsum2(list)
+    max = list[0] # takes into account every number is negative
+    current = list[0]
+    (1...list.length).each do |i|
+        if current + list[i] < 0
+            current = list[i]
+        else
+            current += list[i]
+        end
+        
+        if current > max
+            max = current
+        end
+    end
+    max
+
+
+
+
+    # v1
+    # debugger
+    # (1...list.length).each do |i|
+    #     if current[0] + list[i] > current.sum
+    #         current << list[i]
+    #     else
+    #         current = [list[i]]
+    #     end
+
+    #     max = current if current.sum > max.sum
+    # end
+    # max.sum
+    # max = current if current.sum == max.sum
+end
+
+# 2 3 5 -7 10 -20 3 5 7 15
+# 2 3 5 [-7] 10 [-20 #set our being to -20index + 1] 
+# begin variable to track beginning of sublist
+#                 current = 3
+#                             8, 15, 15= > 30
+#     max sum10 [-3-] [15]
+#     [13]
+# 2 3 5 -7 10 -20 3 5 7 15
+# max = 2
+# current = 2
+# current = 5
+# max <=> current max = 5
+# 10
+# 3
+# 13
+# -20 => -7
+# reset our current
+
+
+
+#  2 3 -7 => -2
 list = [5, 3, -7]
-p largest_contiguous_subsum1(list) # => 8
+# p largest_contiguous_subsum1(list) # => 8
+p largest_contiguous_subsum2(list) # => 8
+
+arr = %w{2 3 5 -7 10 -20 3 5 7 15}.map {|ele| ele.to_i}
+p largest_contiguous_subsum2(arr)
