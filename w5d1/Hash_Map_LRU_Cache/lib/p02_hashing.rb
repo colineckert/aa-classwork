2c7ba382a926d2417e2c8ce5f45ae6640bad4f2e
colineckert <colineckert10@gmail.com>
2020-12-14 16:05:04 -0800

    Finish String and Array hash functions

--- w5d1/Hash_Map_LRU_Cache/lib/p02_hashing.rb
+++ w5d1/Hash_Map_LRU_Cache/lib/p02_hashing.rb
@@ -5,12 +5,19 @@ end
 class Array
   #iterate through arr, call .to_s on ele to get binary => XOR between ele & arr.length
   def hash
-    self.inject {|acc, ele| acc ^ ele }
+    if self.empty?
+      self.object_id.hash 
+    else
+      self.inject {|acc, ele| acc ^ self.index(ele) }.hash
+    end
   end
 end
 
 class String
   def hash
+    char_array = self.split("")
+    new_arr = char_array.map(&:ord)
+    new_arr.hash
   end
 end
 
