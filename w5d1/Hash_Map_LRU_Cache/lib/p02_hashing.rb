0fc1246c92c3bf96eaa4b0b1203ac47746df9748
Brandon Estano <estanob@gmail.com>
2020-12-14 16:29:06 -0800

    Complete Hashing

--- w5d1/Hash_Map_LRU_Cache/lib/p02_hashing.rb
+++ w5d1/Hash_Map_LRU_Cache/lib/p02_hashing.rb
@@ -5,10 +5,8 @@ end
 class Array
   #iterate through arr, call .to_s on ele to get binary => XOR between ele & arr.length
   def hash
-    if self.empty?
-      self.object_id.hash 
-    else
-      self.inject {|acc, ele| acc ^ self.index(ele) }.hash
+    self.each_with_index.inject(0) do |acc_hash, (ele, idx)|
+      (ele.hash + idx.hash) ^ acc_hash
     end
   end
 end
@@ -25,6 +23,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort_by(&:hash).hash
   end
 end
