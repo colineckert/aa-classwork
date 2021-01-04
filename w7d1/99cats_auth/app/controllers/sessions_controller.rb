b68d264ebafa07ce4d153f3c1ee1c6d67d8385bb
colineckert <colineckert10@gmail.com>
2021-01-04 15:07:18 -0800

    Finish users and sessions controllers

--- w7d1/99cats_auth/app/controllers/sessions_controller.rb
+++ w7d1/99cats_auth/app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+      render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:user_name], 
+      params[:user][:password])
+    
+    if @user.nil?
+      render :new
+    else
+      @user.reset_session_token!
+      session[:session_token] = @user.session_token
+      redirect_to cats_url
+    end
+  end
+
+  def destroy
+  end
 end
