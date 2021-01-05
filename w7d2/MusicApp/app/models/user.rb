c7933e208a3695117a5f4fa3ba268fe92a88f1e2
AlexeyS <xengawe@gmail.com>
2021-01-05 11:23:54 -0800

    add user && session controllers

--- w7d2/MusicApp/app/models/user.rb
+++ w7d2/MusicApp/app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
+    attr_reader :password
+
     validates :email, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true 
+
+    after_initialize :ensure_session_token
 
     # S
     # P
     # I
-    # R
-    # E
+    # R +
+    # E +
 
     def self.generate_session_token
         SecureRandom::urlsafe_base64
@@ -22,4 +27,17 @@ class User < ApplicationRecord
         self.session_token
     end
 
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(pw)
+        BCrypt::Password.new(self.password_digest).is_password?(pw)
+    end
+
+    def self.find_by_credential(email, pw)
+        user = User.find_by(email: email)
+        user && user.is_password?(pw) ? user : nil
+    end
 end
