bc2b68603e46d8b5af8f38fd4408d48c01437875
colineckert <colineckert10@gmail.com>
2021-01-11 09:54:49 -0800

    Complete users and sessions controllers

--- w8d1/RedditClone/app/controllers/users_controller.rb
+++ w8d1/RedditClone/app/controllers/users_controller.rb
@@ -1,2 +1,57 @@
 class UsersController < ApplicationController
+  before_action :require_logged_in, except: [:new, :create]
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def index
+    @users = User.all 
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+
+    if @user.update(user_params)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.delete
+    redirect_to users_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
