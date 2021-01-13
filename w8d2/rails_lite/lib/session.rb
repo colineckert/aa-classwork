a2a6fd6e78b75e9305a092f5c7f13e59935125ed
colineckert <colineckert10@gmail.com>
2021-01-12 18:17:54 -0800

    Finish phase 3 of Rails Lite project

---
+++ w8d2/rails_lite/lib/session.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+class Session
+  # find the cookie for this app
+  # deserialize the cookie into a hash
+  def initialize(req)
+    cookie = req.cookies["_rails_lite_app"]
+    if cookie
+      @session_data = JSON.parse(cookie)
+    else
+      @session_data = {}
+    end
+  end
+
+  def [](key)
+   @session_data[key]
+  end
+
+  def []=(key, val)
+   @session_data[key] = val
+  end
+
+  # serialize the hash into json and save in a cookie
+  # add to the responses cookies
+  def store_session(res)
+    cookie = { path: "/", value: @session_data.to_json }
+    res.set_cookie("_rails_lite_app", cookie)
+  end
+end
