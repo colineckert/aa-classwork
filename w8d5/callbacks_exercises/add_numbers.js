480f42f37756045e5d93b9369220695a5309be6e
James Koenig <Koenig.James@Live.com>
2021-01-15 18:02:20 -0800

    wrapping up work for the day

--- w8d5/callbacks_exercises/add_numbers.js
+++ w8d5/callbacks_exercises/add_numbers.js
@@ -5,24 +5,21 @@ const reader = readline.createInterface({
   output: process.stdout
 });
 
-
 function addNumbers(sum, numsLeft, completionCallback) {
-
-  function getNumber(answer) {
-    // sum += parseInt(answer);
-    sum += 1;
+  if (numsLeft > 0) {
+    reader.question("Enter number: ", function(numString){
+      const num = parseInt(numString);
+      sum += num;
+      console.log(`Sum is ${sum}`);
+      addNumbers(sum, numsLeft-1, completionCallback);
+    });
+  } else {
+    completionCallback(sum);
   }
-
-  while (numsLeft > 0) {
-    reader.question("Enter number", getNumber);
-    console.log(3)
-    numsLeft--;
-  }
-  
-  return completionCallback(sum);
 }
 
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
-
-reader.close();
-process.exit();
+addNumbers(0, 3, function(sum) {
+  console.log(`total is: ${sum}`);
+  reader.close();
+  process.exit();
+});
