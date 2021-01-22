963defc3fbbeb7de4af7f0c41ec61a92c501d87f
colineckert <colineckert10@gmail.com>
2021-01-21 16:29:44 -0800

    Complete FollowToggle#handleClick

--- w9d4/AJAX_Twitter/frontend/follow_toggle.js
+++ w9d4/AJAX_Twitter/frontend/follow_toggle.js
@@ -1,10 +1,37 @@
 class FollowToggle {
   constructor(el) {
-    this.$el = $(el);
+    this.$el = el;
     this.userId = this.$el.data("user-id");
     this.followState = this.$el.data("initial-follow-state");
-    debugger
+    
+    this.render();
+    this.$el.on("click", this.handleClick.bind(this));
   }
+
+  render() {
+    if (this.followState === "unfollowed") {
+      this.$el.text("Follow!");
+    } else if (this.followState === "followed") {
+      this.$el.text("Unfollow!");
+    };
+  }
+
+  handleClick(e) {
+    const followToggle = this;
+    e.preventDefault();
+    
+    return $.ajax({
+      method: (followToggle.followState === "unfollowed") ? 'POST' : 'DELETE', 
+      url: `/users/${followToggle.userId}/follow`,
+      dataType: 'JSON',
+      success: function(){
+        followToggle.followState = followToggle.followState === "unfollowed" ? "followed" : "unfollowed";
+        followToggle.render();
+      }
+    });
+  }
+
 }
+
 
 module.exports = FollowToggle;
