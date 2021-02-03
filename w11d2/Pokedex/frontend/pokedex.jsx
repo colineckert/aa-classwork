ed828939976dba3237ae78f5c26dd47c6edd65f2
colineckert <colineckert10@gmail.com>
2021-02-02 17:03:56 -0800

    Create frontend structure

--- w11d2/Pokedex/frontend/pokedex.jsx
+++ w11d2/Pokedex/frontend/pokedex.jsx
@@ -6,5 +6,5 @@ import { fetchAllPokemon } from './util/api_util';
 document.addEventListener('DOMContentLoaded', () => {
   window.fetchAllPokemon = fetchAllPokemon;
   const rootEl = document.getElementById('root');
-  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  ReactDOM.render(<h1>Pokedex</h1C>, rootEl);
 });
