cf5b5b6626457af8346657be54afb5ce296ae672
colineckert <colineckert10@gmail.com>
2021-03-02 12:59:36 -0800

    complete climb stairs

--- w15d2/tabulation_project/lib/leet_code_64.js
+++ w15d2/tabulation_project/lib/leet_code_64.js
@@ -2,5 +2,13 @@
 // Feel free to use this file for scratch work.
 
 function minPathSum(grid) {
-
+  let m = grid.length;
+  let n = grid[0].length;
+  let table = new Array(m).fill(new Array(n));
+  
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      
+    }
+  }
 }
