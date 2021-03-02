47d3f474e8d0ca578ed63e0ce5f36aa3540dd632
colineckert <colineckert10@gmail.com>
2021-03-02 15:00:24 -0800

    Finish selection sort

---
+++ w15d2/selection_sort_project/lib/selection_sort.js
@@ -0,0 +1,23 @@
+function swap(arr, index1, index2) {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+    return arr;
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+    swap(arr, i, minIndex);
+  }
+  return arr;
+}
+
+module.exports = {
+    selectionSort,
+    swap
+};
