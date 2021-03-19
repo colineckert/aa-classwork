048df958b81d4bb31fd48e5a9aa85884e711cec1
colineckert <colineckert10@gmail.com>
2021-03-19 13:23:07 -0700

    Add helper methods

--- w16d1/radix_sort_project/lib/radix_sort.js
+++ w16d1/radix_sort_project/lib/radix_sort.js
@@ -1,5 +1,17 @@
-function radixSort(arr) {
+const getDigitFrom = (num, place) => Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;;
 
+const getIntLength = (num) => (num === 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+
+function getMaxDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
+  }
+  return maxDigits;
+}
+
+function radixSort(arr) {
+    
 }
 
 module.exports = {
