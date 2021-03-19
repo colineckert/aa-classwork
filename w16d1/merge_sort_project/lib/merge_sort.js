91fb73531beb1d73b2915ea4b33504947b832d6b
colineckert <colineckert10@gmail.com>
2021-03-19 13:03:16 -0700

    Finish merge sort

--- w16d1/merge_sort_project/lib/merge_sort.js
+++ w16d1/merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,28 @@
 function merge(array1, array2) {
+    const merged = [];
 
+    while (array1.length && array2.length) {
+        if (array1[0] < array2[0]) {
+            merged.push(array1.shift());
+        } else {
+            merged.push(array2.shift());
+        }
+    }
+
+    return merged.concat(array1, array2);
 }
 
 function mergeSort(array) {
+    if (array.length <= 1) return array;
 
+    let midIdx = Math.floor(array.length / 2);
+    let left = array.slice(0, midIdx);
+    let right = array.slice(midIdx);
+
+    let sortedLeft = mergeSort(left);
+    let sortedRight = mergeSort(right);
+
+    return merge(sortedLeft, sortedRight);
 }
 
 module.exports = {
