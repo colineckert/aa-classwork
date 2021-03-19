a60c887aff5358a9520981fcff5028942b246573
colineckert <colineckert10@gmail.com>
2021-03-19 15:32:59 -0700

    Finish binary search

--- w16d1/binary_search_project/lib/binary_search.js
+++ w16d1/binary_search_project/lib/binary_search.js
@@ -1,9 +1,34 @@
 function binarySearch(array, target) {
+    if (!array.length) return false;
 
+    let midIdx = Math.floor(array.length / 2);
+    let left = array.slice(0, midIdx);
+    let right = array.slice(midIdx + 1);
+
+    if (target < array[midIdx]) {
+        return binarySearch(left, target);
+    } else if (target > array[midIdx]) {
+        return binarySearch(right, target) 
+    } else {
+        return true;
+    }
 }
 
 function binarySearchIndex(array, target) {
+    if (!array.length) return -1;
 
+    let midIdx = Math.floor(array.length / 2);
+    let left = array.slice(0, midIdx);
+    let right = array.slice(midIdx + 1);
+
+    if (target < array[midIdx]) {
+        return binarySearchIndex(left, target);
+    } else if (target > array[midIdx]) {
+        let searchRight = binarySearchIndex(right, target);
+        return searchRight === -1 ? -1 : searchRight + midIdx + 1;
+    } else {
+        return midIdx;
+    }
 }
 
 
