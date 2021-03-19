49696251b136f0e4571e1a3ab74770e487fdb334
colineckert <colineckert10@gmail.com>
2021-03-19 15:14:18 -0700

    Finish radix sort'

--- w16d1/radix_sort_project/lib/radix_sort.js
+++ w16d1/radix_sort_project/lib/radix_sort.js
@@ -11,7 +11,21 @@ function getMaxDigits(nums) {
 }
 
 function radixSort(arr) {
-    
+    if (!Array.isArray(arr)) return null;
+
+    let maxDigits = getMaxDigits(arr);
+    for (let k = 0; k < maxDigits; k++) {
+        let buckets = Array.from({ length: 10 }, () => []);
+
+        for (let i = 0; i < arr.length; i++) {
+            let digit = getDigitFrom(arr[i], k);
+            buckets[digit].push(arr[i]);
+        }
+
+        arr = [].concat(...buckets);
+    }
+
+    return arr;
 }
 
 module.exports = {
