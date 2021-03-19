94105da3a1b5ae121e430547d492f101dbedcc2e
colineckert <colineckert10@gmail.com>
2021-03-19 15:19:59 -0700

    Finish counting sort

--- w16d1/counting_sort_project/lib/counting_sort.js
+++ w16d1/counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,19 @@
 function countingSort(arr, max) {
+    const result = [];
+    const counters = new Array(max + 1).fill(0);
 
+    for (let i = 0; i < arr.length; i++) {
+        counters[arr[i]]++;
+    }
+
+    for (let k = 0; k < counters.length; k++) {
+        while (counters[k] > 0) {
+            result.push(k);
+            counters[k]--;
+        }
+    }
+
+    return result;
 }
 
 
