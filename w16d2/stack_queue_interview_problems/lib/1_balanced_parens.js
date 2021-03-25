ea962fbe33018780f48ed3bff290cb3635e71ce9
colineckert <colineckert10@gmail.com>
2021-03-25 15:44:47 -0700

    Finish balanced parens

--- w16d2/stack_queue_interview_problems/lib/1_balanced_parens.js
+++ w16d2/stack_queue_interview_problems/lib/1_balanced_parens.js
@@ -76,7 +76,24 @@
 // Let's code!
 // -----------
 function balancedParens(str) {
+  const stack = [];
+  const pairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  }
 
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (pairs[char]) {
+      stack.push(char);
+    } else if (char === ')' || char === ']' | char === '}') {
+      if (pairs[stack.pop()] !== char) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
 
 exports.balancedParens = balancedParens;
