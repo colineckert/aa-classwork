ff493191fe4dc027d560542230f8ec5d0278e56d
colineckert <colineckert10@gmail.com>
2021-03-25 18:01:07 -0700

    Finish iterate over linked list backwards

--- w16d2/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
+++ w16d2/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
@@ -28,7 +28,71 @@
 
 function iterateAcrossLinkedListBackwards(linkedList) {
     // TODO: Implement the iterateAcrossLinkedListBackwards function here
+    const stack = new Stack();
+    let current = linkedList.head;
+    let result = '';
 
+    while (current !== null) {
+        stack.push(current);
+        current = current.next;
+    }
+
+    while (stack.size() > 0) {
+        if (stack.size() > 1) {
+            result += `${(stack.pop().value)} -> `;
+        } else {
+            result += `${(stack.pop().value)}`;
+        }
+    }
+
+    return result;
+}
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        const newNode = new Node(val);
+
+        if (!this.top) {
+            this.top = newNode;
+            this.bottom= newNode;
+        } else {
+            const temp = this.top;
+            this.top = newNode;
+            this.top.next = temp;
+        }
+
+        return ++this.length;
+    }
+
+    pop() {
+        if (!this.top) {
+            return null;
+        } 
+        const temp = this.top;
+        if (this.top === this.bottom) {
+            this.bottom = null;
+        }
+        this.top = this.top.next;
+        this.length--;
+        return temp.value;
+    }
+
+    size() {
+        return this.length;
+    }
 }
 
 exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards;
