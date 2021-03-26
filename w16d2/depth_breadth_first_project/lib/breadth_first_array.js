064cec960aa610484714bd7935b717aff28d2687
colineckert <colineckert10@gmail.com>
2021-03-26 15:59:37 -0700

    Complete depth and breadth first

--- w16d2/depth_breadth_first_project/lib/breadth_first_array.js
+++ w16d2/depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,14 @@
 function breadthFirstArray(root) {
+    const queue = [root];
+    const visited = [];
 
+    while (queue.length) {
+        let node = queue.pop();
+        visited.push(node.val);
+        if (node.left) queue.unshift(node.left);
+        if (node.right) queue.unshift(node.right);
+    }
+    return arr;
 }
 
 module.exports = {
