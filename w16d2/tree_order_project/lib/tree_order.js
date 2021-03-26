c8dc6c32eb0f5875312f9d88002de37a94f4b37c
colineckert <colineckert10@gmail.com>
2021-03-26 12:56:39 -0700

    Complete leetcode 105

--- w16d2/tree_order_project/lib/tree_order.js
+++ w16d2/tree_order_project/lib/tree_order.js
@@ -1,9 +1,19 @@
 function inOrderArray(root) {
-
+    if (!root) return [];
+    return [
+        ...inOrderArray(root.left),
+        root.val,
+        ...inOrderArray(root.right)
+    ];
 }
 
 function postOrderArray(root) {
-
+    if (!root) return [];
+    return [
+        ...postOrderArray(root.left),
+        ...postOrderArray(root.right),
+        root.val
+    ];
 }
 
 
