8098e77e7bf9148482a453ba70ca9f51591db476
colineckert <colineckert10@gmail.com>
2021-03-26 16:49:30 -0700

    Complete tree height

--- w16d2/depth_breadth_first_project/lib/tree_height.js
+++ w16d2/depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,24 @@
-function treeHeight(root) {
+// function treeHeight(root) {
+//     if (!root) return -1;
 
+//     let maxDepth = 0;
+//     const stack = [{node: root, depth: 0}];
+
+//     while (stack.length) {
+//         let { node, depth } = stack.pop();
+        
+//         if (depth >= maxDepth) maxDepth = depth;
+
+//         if (node.right) stack.push({ node: node.right, depth: depth + 1 });
+//         if (node.left) stack.push({ node: node.left, depth: depth + 1 });
+//     }
+
+//     return maxDepth;
+// }
+
+function treeHeight(root) {
+    if (!root) return -1;
+    return 1 + Math.max(treeHeight(root.left), treeHeight(root.right));
 }
 
 
