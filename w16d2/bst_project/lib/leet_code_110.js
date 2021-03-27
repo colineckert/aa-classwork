6bceb448e7922151d59da35505e0f45ddc67ae55
colineckert <colineckert10@gmail.com>
2021-03-27 16:27:43 -0700

    Finish leetcode 110

--- w16d2/bst_project/lib/leet_code_110.js
+++ w16d2/bst_project/lib/leet_code_110.js
@@ -1,7 +1,13 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
+function treeHeight(root) {
+    if (!root) return -1;
+    return 1 + Math.max(treeHeight(root.left), treeHeight(root.right));
+}
 
 function isBalanced(root) {
-
+    if (!root) return true;
+    let heightDiff = Math.abs(treeHeight(root.left) - treeHeight(root.right)) <= 1;
+    return heightDiff && isBalanced(root.left) && isBalanced(root.right);
 }
