92bbb5b33aa74a3692f86ecd582c5586a3dfc8a9
colineckert <colineckert10@gmail.com>
2021-03-27 15:55:42 -0700

    Finish BST iterative and recursive search

--- w16d2/bst_project/lib/bst.js
+++ w16d2/bst_project/lib/bst.js
@@ -8,7 +8,78 @@ class TreeNode {
 
 
 class BST {
+   constructor() {
+       this.root = null;
+   }
+
+   insert(val, root = this.root) {
+        if (!this.root) {
+            this.root = new TreeNode(val);
+            return;
+        }
+
+        if (val < root.val) {
+            if (!root.left) {
+                root.left = new TreeNode(val);
+            } else {
+                this.insert(val, root.left);
+            }
+        } else {
+            if (!root.right) {
+                root.right = new TreeNode(val);
+            } else {
+                this.insert(val, root.right);
+            }
+        }
+   }
+
    
+
+   searchRecur(val, root = this.root) {
+       if (!root) return false;
+
+       if (val < root.val) {
+           return this.searchRecur(val, root.left)
+       } else if (val > root.val) {
+           return this.searchRecur(val, root.right)
+       } else {
+           return true;
+       }
+   }
+// this still has O(n) space with the queue
+//    searchIter(val) { 
+//         if (!this.root) return false;
+
+//         let queue = [this.root];
+//         while (queue.length) {
+//             let node = queue.shift();
+//             if (val < node.val) {
+//                 if (node.left) queue.push(node.left);
+//             } else if (val > node.val) {
+//                 if (node.right) queue.push(node.right);
+//             } else {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//    }
+
+// O(1) space
+   searchIter(val) {  
+       let currNode = this.root;
+       while (currNode) {
+            if (val < currNode.val) {
+                currNode = currNode.left;
+            } else if (val > currNode.val) {
+                currNode = currNode.right;
+            } else {
+                return true;
+            }
+       }
+
+       return false;
+   }
 }
 
 module.exports = {
