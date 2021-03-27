1104b986512e93179a5856f7c291eb88c540fd1f
colineckert <colineckert10@gmail.com>
2021-03-27 16:53:07 -0700

    Complete leetcode 108

--- w16d2/bst_project/lib/leet_code_108.js
+++ w16d2/bst_project/lib/leet_code_108.js
@@ -3,5 +3,16 @@
 
 
 function sortedArrayToBST(nums) {
-
+    if (!nums.length) return null;
+    
+    let midIdx = Math.floor(nums.length / 2);
+    let root = new TreeNode(nums[midIdx]);
+    
+    let leftTree = sortedArrayToBST(nums.slice(0, midIdx));
+    let rightTree = sortedArrayToBST(nums.slice(midIdx + 1));
+    
+    root.left = leftTree;
+    root.right = rightTree;
+    
+    return root;
 }
