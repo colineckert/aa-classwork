070532c877b4b1fdc0414cc600ea194c6003c30f
colineckert <colineckert10@gmail.com>
2021-03-29 20:23:30 -0700

    Finish max heap implementation

--- w16d3/heaps_project/lib/max_heap.js
+++ w16d3/heaps_project/lib/max_heap.js
@@ -1,5 +1,65 @@
 class MaxHeap {
+    constructor() {
+        this.array = [null];
+    }
+
+    getParent(idx) {
+        return Math.floor(idx / 2);
+    }
+
+    getLeftChild(idx) {
+        return idx * 2;
+    }
+
+    getRightChild(idx) {
+        return idx * 2 + 1;
+    }
+
+    siftUp(idx) {
+        if (idx === 1) return;
+
+        let parentIdx = this.getParent(idx);
+
+        if (this.array[parentIdx] < this.array[idx]) {
+            [ this.array[parentIdx], this.array[idx] ] = [ this.array[idx], this.array[parentIdx] ]
+            this.siftUp(parentIdx);
+        }
+    }
+
+    insert(val) {
+        this.array.push(val);
+        this.siftUp(this.array.length - 1);
+    }
+
+    siftDown(idx) {
+        let arr = this.array;
+        let leftIdx = this.getLeftChild(idx);
+        let rightIdx = this.getRightChild(idx);
+        let leftVal = arr[leftIdx];
+        let rightVal = arr[rightIdx];
+
+        if (leftVal === undefined) leftVal = -Infinity;
+        if (rightVal === undefined) rightVal = -Infinity;
+
+        if (arr[idx] > leftVal && arr[idx] > rightVal) return;
+
+        let swapIdx = leftVal > rightVal ? leftIdx : rightIdx;
+
+        [ arr[idx], arr[swapIdx] ] = [ arr[swapIdx], arr[idx] ];
+
+        this.siftDown(swapIdx);
+    }
     
+    deleteMax() {
+        if (this.array.length === 2) return this.array.pop();
+        if (this.array.length === 1) return null;
+
+        let max = this.array[1];
+        this.array[1] = this.array.pop();
+
+        this.siftDown(1);
+        return max;
+    }
 }
 
 module.exports = {
