04c64246efb220d3b326c5a78bf28c0f187dd161
colineckert <colineckert10@gmail.com>
2021-04-03 20:46:07 -0700

    add optimal three sum

---
+++ leetcode/medium/three_sum.js
@@ -0,0 +1,26 @@
+// O(n^2) time | O(n) space
+function threeNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+	
+	const triSums = [];
+	
+	for (let i = 0; i < array.length; i++) {
+		let left = i + 1;
+		let right = array.length - 1;
+		
+		while (left < right) {
+			let currentSum = array[i] + array[left] + array[right];
+			if (currentSum === targetSum) {
+				triSums.push([array[i], array[left], array[right]]);
+				left++;
+				right--;
+			} else if (currentSum < targetSum) {
+				left++;
+			} else if (currentSum > targetSum) {
+				right--;
+			}
+		}
+	}
+	
+	return triSums;
+}
