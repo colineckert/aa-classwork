014038c5935ccf5c0585ffba83889c339e02b745
colineckert <colineckert10@gmail.com>
2021-04-05 17:00:54 -0700

    refactor solution

--- leetcode/medium/move_element_to_end.js
+++ leetcode/medium/move_element_to_end.js
@@ -4,14 +4,10 @@ function moveElementToEnd(array, toMove) {
 	let j = array.length - 1;
 	while (i < j) {
 		while (i < j && array[j] === toMove) j--;
-		if (array[i] === toMove) swap(i, j, array);
+		if (array[i] === toMove) {
+			[array[i], array[j]] = [array[j], array[i]]
+ 		}
 		i++;
 	}
 	return array;
-}
-
-function swap(i, j, array) {
-	const temp = array[j];
-	array[j] = array[i];
-	array[i] = temp;
 }
