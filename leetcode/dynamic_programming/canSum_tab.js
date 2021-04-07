534d7d8bdbd5e842bd74af87b5adda46f20e34e9
colineckert <colineckert10@gmail.com>
2021-04-07 15:34:11 -0700

    add canSum tabulated

---
+++ leetcode/dynamic_programming/canSum_tab.js
@@ -0,0 +1,14 @@
+// O(m*n) time | O(m) space - where m is the targetSum
+const canSum = (targetSum, numbers) => {
+  const table = Array(targetSum + 1).fill(false);
+  table[0] = true;
+  for (let i = 0; i <= targetSum; i++) {
+    if (table[i] === true) {
+      for (let num of numbers) {
+        table[i + num] = true;
+      }
+    }
+  }
+
+  return table[targetSum];
+}
