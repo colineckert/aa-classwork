ad46f998879979f0fe5129088d5a4e9dfdc89e1c
colineckert <colineckert10@gmail.com>
2021-04-06 21:21:08 -0700

    new folder structure

---
+++ leetcode/dynamic_programming/canConstruct.js
@@ -0,0 +1,22 @@
+// m = target.length -> height of tree
+// n = wordBank.length
+
+// O(n * m^2) time | O(m^2) space
+const canConstruct = (target, wordBank, memo) => {
+  if (target in memo) return memo[target];
+  if (target === '') return true;
+
+  for (let word of wordBank) {
+    // checks if the word starts at index 0
+    if (target.indexOf(word) === 0) {
+      // slice off the prefix 
+      const suffix = target.slice(word.length);
+      if (canConstruct(suffix, wordBank, memo)) {
+        memo[target] = true;
+        return true;
+      }
+    }
+  }
+  memo[target] = false;
+  return false;
+}
