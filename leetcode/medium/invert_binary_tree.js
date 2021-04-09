f1defbbdeaf2cd1fa561c477db9610528fa1f22b
colineckert <colineckert10@gmail.com>
2021-04-08 19:10:06 -0700

    simplified invert BT

--- leetcode/medium/invert_binary_tree.js
+++ leetcode/medium/invert_binary_tree.js
@@ -13,10 +13,8 @@ function invertBinaryTree(tree) {
 
 // shorter version
 function invertBinaryTree(tree) {
-  if (tree === null) return;
-
+  if (!tree) return;
 	[tree.left, tree.right] = [tree.right, tree.left];
-
 	invertBinaryTree(tree.left);
 	invertBinaryTree(tree.right);
 }
