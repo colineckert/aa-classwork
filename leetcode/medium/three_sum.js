774ca4ab4ec253c9a80ac09110cfeb683c714a78
colineckert <colineckert10@gmail.com>
2021-05-03 09:56:29 -0700

    add alt three sum

--- leetcode/medium/three_sum.js
+++ leetcode/medium/three_sum.js
@@ -24,3 +24,33 @@ function threeNumberSum(array, targetSum) {
 	
 	return triSums;
 }
+
+// alt solution to avoid duplicate values
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let triplets = [];
+    for (let i = 0; i < nums.length; i ++) {
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
+        findPair(nums, -nums[i], i + 1, triplets);
+    }
+    
+    return triplets;
+};
+
+const findPair = (arr, target, left, triplets) => {
+    let right = arr.length - 1;
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === target) {
+            triplets.push([-target, arr[left], arr[right]])
+            left += 1;
+            right -= 1;
+            while (left < right && arr[left] === arr[left - 1]) left += 1;
+            while (left < right && arr[right] === arr[right + 1]) right -= 1;
+        } else if (sum < target) {
+            left += 1;   
+        } else {
+            right -= 1;
+        }
+    }
+}
