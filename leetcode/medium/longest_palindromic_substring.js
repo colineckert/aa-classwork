f1310d3949d29bc9f944e18e6a41cba06bfb6a13
colineckert <colineckert10@gmail.com>
2021-05-04 11:48:11 -0700

    add optimal longest palindromic substring

--- leetcode/medium/longest_palindromic_substring.js
+++ leetcode/medium/longest_palindromic_substring.js
@@ -22,3 +22,23 @@ function isPalindrome(string) {
 	return true;
 }
 
+// O(n^2) time | O(n) space
+function longestPalindromicSubstring(string) {
+	let currLongest = [0, 1];
+	for (let i = 0; i < string.length; i++) {
+		const odd = getLongestPalindrome(string, i - 1, i + 1);
+		const even = getLongestPalindrome(string, i - 1, i);
+		const longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+		currLongest = currLongest[1] - currLongest[0] > longest[1] - longest[0] ? currLongest : longest;
+	}
+	return string.slice(currLongest[0], currLongest[1]);
+}
+
+function getLongestPalindrome(string, leftIdx, rightIdx) {
+	while (leftIdx >= 0 && rightIdx < string.length) {
+		if (string[leftIdx] !== string[rightIdx]) break;
+		leftIdx--;
+		rightIdx++;
+	}
+	return [leftIdx + 1, rightIdx];
+}
