95880c51488f4f6394e07fb14275b699deebab4f
colineckert <colineckert10@gmail.com>
2021-05-04 10:23:09 -0700

    add solution 2 to cycle graph

--- leetcode/medium/cycle_in_graph.js
+++ leetcode/medium/cycle_in_graph.js
@@ -1,3 +1,4 @@
+// SOLUTION 1
 // O(v + e) time | O(v) space - where v is the number of vertices
 // and e is the number of edges in the graph
 function cycleInGraph(edges) {
@@ -30,5 +31,43 @@ function isNodeInCycle(node, edges, visited, inStack) {
 	}
 	
 	inStack[node] = false;
+	return false;
+}
+
+// SOLUTION 2
+const [WHITE, GREY, BLACK] = [0, 1, 2];
+
+// O(v + e) time | O(v) space - where v is the number of vertices
+// and e is the number of edges in the graph
+function cycleInGraph(edges) {
+  const numberOfNodes = edges.length
+	const colors = new Array(numberOfNodes).fill(WHITE);
+	
+	for (let node = 0; node < numberOfNodes; node++) {
+		if (colors[node] != WHITE) continue
+		
+		const containsCycle = traverseAndColorNodes(node, edges, colors);
+		if (containsCycle) return true;
+	}
+	
+  return false;
+}
+
+function traverseAndColorNodes(node, edges, colors) {
+	colors[node] = GREY;
+	
+	const neighbors = edges[node];
+	for (const neighbor of neighbors) {
+		const neighborColor = colors[neighbor];
+		
+		if (neighborColor === GREY) return true;
+    
+		if (neighborColor === BLACK) continue;
+		
+		const containsCycle = traverseAndColorNodes(neighbor, edges, colors);
+		if (containsCycle) return true;
+	}
+	
+	colors[node] = BLACK;
 	return false;
 }
