b6f6f514acb58d2db3658658d4fcea95b0569b23
colineckert <colineckert10@gmail.com>
2021-05-04 21:29:50 -0700

    add solution 2 to permutations

--- leetcode/medium/permutations.js
+++ leetcode/medium/permutations.js
@@ -18,3 +18,29 @@ function permutationsHelper(array, currPermutation, permutations) {
 	}
 }
 
+// O(n*n!) time | O(n*n!) space
+function getPermutations(array) {
+  const permutations = [];
+	permutationsHelper(0, array, permutations);
+	return permutations;
+}
+
+function permutationsHelper(i, array, permutations) {
+	if (i === array.length - 1) {
+		permutations.push([...array]);
+	} else {
+		for (let j = i; j < array.length; j++) {
+			swap(i, j, array);
+			permutationsHelper(i + 1, array, permutations);
+			swap(i, j, array);
+		}
+	}
+}
+
+function swap(i, j, array) {
+	const temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
+// Do not edit the line below.
+exports.getPermutations = getPermutations;
