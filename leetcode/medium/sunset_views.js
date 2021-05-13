771fd1079b7c68b0b8822e5682a7b8363467fe32
colineckert <colineckert10@gmail.com>
2021-05-13 13:07:49 -0700

    Add alt stack sunset view

--- leetcode/medium/sunset_views.js
+++ leetcode/medium/sunset_views.js
@@ -22,3 +22,30 @@ function sunsetViews(buildings, direction) {
 	
 	return buildingsWithSunsetView;
 }
+
+// O(n) time | O(n) space - where n is the length of the input array
+function sunsetViews(buildings, direction) {
+  const candidateBuildings = [];
+	
+	const startIdx = direction === 'EAST' ? 0 : buildings.length - 1;
+	const step = direction === 'EAST' ? 1 : -1;
+	
+	let idx = startIdx;
+	while (idx >= 0 && idx < buildings.length) {
+		const buildingHeight = buildings[idx];
+		
+		while (
+			candidateBuildings.length > 0 && 
+			buildings[candidateBuildings[candidateBuildings.length -1]] <= buildingHeight
+		) {
+			candidateBuildings.pop();
+		}
+		
+		candidateBuildings.push(idx);
+		idx = idx + step;
+	}
+	
+	if (direction === 'WEST') candidateBuildings.reverse();
+	
+	return candidateBuildings;
+}
