69de51384edfb60969c7fc0403429d9b1dcd51f4
colineckert <colineckert10@gmail.com>
2021-05-17 13:32:30 -0700

    update longest substring for LC version

--- leetcode/hard/longest_substring_without_duplication.js
+++ leetcode/hard/longest_substring_without_duplication.js
@@ -21,3 +21,23 @@ function longestSubstringWithoutDuplication(string) {
 
 	return string.slice(longest[0], longest[1]);
 }
+
+// 3. Longest Substring Without Repeating Characters
+// If returning int instead of substring
+// O(n) time | O(min(n, a)) space
+var lengthOfLongestSubstring = function(s) {
+    const lastSeen = {};
+    let longest = 0
+    let startIdx = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (char in lastSeen) {
+            startIdx = Math.max(startIdx, lastSeen[char] + 1);
+        }
+        longest = Math.max(i - startIdx + 1, longest);
+        lastSeen[char] = i;
+    }
+    
+    return longest;
+};
