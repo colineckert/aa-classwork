c0020d1277e2abba52e3b66e7fbbd7af72c64f87
colineckert <colineckert10@gmail.com>
2021-05-20 23:47:16 -0700

    add alt for two city sched

--- leetcode/medium/two_city_scheduling.js
+++ leetcode/medium/two_city_scheduling.js
@@ -1,3 +1,18 @@
+// O(nlog(n)) time | O(1) space
+var twoCitySchedCost = function(costs) {
+    costs.sort((a, b) => (a[0] - a[1]) - (b[0] - b[1]));
+    
+    let total = 0;
+    let n = Math.floor(costs.length / 2);
+ 
+    for (let i = 0; i < n; i++) {
+        total += costs[i][0] + costs[i + n][1];
+    }
+    
+    return total;
+};
+
+// alt - same time/space
 var twoCitySchedCost = function(costs) {
     const n = costs.length / 2;
     costs.sort((a, b) => Math.abs(b[0] - b[1]) - Math.abs(a[0] - a[1]));
