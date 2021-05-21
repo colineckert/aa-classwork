afdd1aba3ccdf50d6ee5c6378d9f7bf825d338f1
colineckert <colineckert10@gmail.com>
2021-05-20 22:29:45 -0700

    add optimized n queens

--- leetcode/hard/non-attacking_queens.js
+++ leetcode/hard/non-attacking_queens.js
@@ -30,3 +30,58 @@ function isNonAttackingPlacement(row, col, columnPlacements) {
 	}
 	return true;
 }
+
+// Upper bound: O(n!) time | O(n) space - where n is the input number
+function nonAttackingQueens(n) {
+	const blockedColumns = new Set();
+	const blockedUpDiagonals = new Set();
+	const blockedDownDiagonals = new Set();
+	return getNumberOfNonAttackingQueenPlacements(0, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, n);
+}
+
+function getNumberOfNonAttackingQueenPlacements(
+	row,
+ 	blockedColumns,
+	blockedUpDiagonals,
+ 	blockedDownDiagonals,
+	boardSize
+) {
+	if (row === boardSize) return 1;
+	
+	let validPlacements = 0;
+	for (let col = 0; col < boardSize; col++) {
+		if (isNonAttackingPlacement(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals)) {
+			placeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals);
+			validPlacements += getNumberOfNonAttackingQueenPlacements(
+				row + 1, 
+				blockedColumns, 
+				blockedUpDiagonals, 
+				blockedDownDiagonals, 
+				boardSize
+			);
+			removeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals);
+		}
+	}
+	return validPlacements;
+}
+
+// This is always an O(1)-time operation
+function isNonAttackingPlacement(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals) {
+	if (blockedColumns.has(col)) return false;
+	if (blockedUpDiagonals.has(row + col)) return false;
+	if (blockedDownDiagonals.has(row - col)) return false;
+	
+	return true;
+}
+
+function placeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals) {
+	blockedColumns.add(col);
+	blockedUpDiagonals.add(row + col);
+	blockedDownDiagonals.add(row - col);
+}
+
+function removeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals) {
+	blockedColumns.delete(col);
+	blockedUpDiagonals.delete(row + col);
+	blockedDownDiagonals.delete(row - col);
+}
