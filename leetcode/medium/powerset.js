aac719223ff45618c25186f4954582dca44d20a2
colineckert <colineckert10@gmail.com>
2021-05-22 15:32:41 -0700

    add string variation to powersets

--- leetcode/medium/powerset.js
+++ leetcode/medium/powerset.js
@@ -1,6 +1,7 @@
 // O(n*2^n) time | O(n*2^n) space
 function powerset(array) {
   const subsets = [[]];
+
 	for (const ele of array) {
 		const length = subsets.length;
 		for (let i = 0; i < length; i++) {
@@ -8,5 +9,36 @@ function powerset(array) {
 			subsets.push(currentSubset.concat(ele));
 		}
 	}
+
 	return subsets;
 }
+
+/*
+Variation for string powersets
+Eg. String "abc" should output
+empty string
+a
+b
+c
+ab
+bc
+ac
+abc
+ */
+
+function stringSubsets(str) {
+  const subsets = [''];
+
+  for (const char of str) {
+    let length = subsets.length;
+    for (let i = 0; i < length; i++) {
+      let current = subsets[i];
+      subsets.push(current += char);
+    }
+  }
+	
+  return subsets;
+}
+
+let str = "abc";
+console.log(stringSubsets(str));
