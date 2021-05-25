c55e82d66965eff563d1de8bee5fa9fa7f901a49
colineckert <colineckert10@gmail.com>
2021-05-25 16:11:53 -0700

    Add water area

--- leetcode/42.trapping-rain-water.js
+++ leetcode/42.trapping-rain-water.js
@@ -10,7 +10,28 @@
  * @return {number}
  */
 var trap = function(height) {
-    
+  if (!height.length) return 0;
+
+  const length = height.length;
+  const leftMax = {};
+  const rightMax = {};
+  let area = 0;
+
+  leftMax[0] = height[0];
+  for (let i = 1; i < length; i++) {
+    leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+  }
+
+  rightMax[length - 1] = height[length - 1];
+  for (let i = length - 2; i >= 0; i--) {
+    rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+  }
+
+  for (let i = 0; i < length; i++) {
+    area += Math.min(leftMax[i], rightMax[i]) - height[i];
+  }
+
+  return area;
 };
 // @lc code=end
 
