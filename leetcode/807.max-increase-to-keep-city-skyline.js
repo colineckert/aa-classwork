0bde7919ed867614b52410ee94f647c2927e8191
colineckert <colineckert10@gmail.com>
2021-05-25 10:31:43 -0700

    Add skyline

---
+++ leetcode/807.max-increase-to-keep-city-skyline.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=807 lang=javascript
+ *
+ * [807] Max Increase to Keep City Skyline
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxIncreaseKeepingSkyline = function(grid) {
+	const rowMaxes = {};
+	const colMaxes = {};
+	let diff = 0;
+
+	for (let r = 0; r < grid.length; r++) {
+		for (let c = 0; c < grid[r].length; c++) {
+			if (!(rowMaxes[r])) {
+				rowMaxes[r] = grid[r][c];
+			} else {
+				rowMaxes[r] = Math.max(grid[r][c], rowMaxes[r]);
+			}
+
+			if (!(colMaxes[c])) {
+				colMaxes[c] = grid[r][c];
+			} else {
+				colMaxes[c] = Math.max(grid[r][c], colMaxes[c]);
+			}
+
+		}
+	}
+
+
+	for (let r = 0; r < grid.length; r++) {
+		for (let c = 0; c < grid[r].length; c++) {
+			let value = grid[r][c];
+			const rowMax = rowMaxes[r];
+			const colMax = colMaxes[c];
+			const posMax = Math.min(rowMax, colMax);
+			diff += (posMax - value);
+		}
+	}
+
+	return diff;
+};
+// @lc code=end
+
