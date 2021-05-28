b3e06bd5c41436b9297210dafd86e8e61aee08ee
colineckert <colineckert10@gmail.com>
2021-05-28 11:15:13 -0700

    update coin change

--- leetcode/322.coin-change.js
+++ leetcode/322.coin-change.js
@@ -16,13 +16,13 @@ var coinChange = function(coins, amount) {
   const numCoins = new Array(amount + 1).fill(Infinity);
   numCoins[0] = 0;
     
-    for (const coin of coins) {
-      for (let amt = 0; amt < numCoins.length; amt++) {
-        if (coin <= amt) {
-          numCoins[amt] = Math.min(numCoins[amt], numCoins[amt - coin] + 1);
-        } 
-      }
+  for (const coin of coins) {
+    for (let amt = 0; amt < numCoins.length; amt++) {
+      if (coin <= amt) {
+        numCoins[amt] = Math.min(numCoins[amt], numCoins[amt - coin] + 1);
+      } 
     }
+  }
     
   return numCoins[amount] === Infinity ? -1 : numCoins[amount];
 };
